2c0e80bcdc226e2b78938f1882de5a8ec63654a7
frodesundby <frode.sundby@nav.no>
2021-06-15 14:25:13 +0200

    Checking for gcp team project before creating gcp resources

--- pkg/resourcecreator/google/gcp/gcp.go
+++ pkg/resourcecreator/google/gcp/gcp.go
@@ -1,14 +1,15 @@
 package gcp
 
 import (
-	nais_io_v1alpha1 "github.com/nais/liberator/pkg/apis/nais.io/v1alpha1"
-	"github.com/nais/naiserator/pkg/resourcecreator/google/iam"
-	"github.com/nais/naiserator/pkg/resourcecreator/google/sql"
-	"github.com/nais/naiserator/pkg/resourcecreator/google/storagebucket"
+	"fmt"
+	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
+	google_iam "github.com/nais/naiserator/pkg/resourcecreator/google/iam"
+	google_sql "github.com/nais/naiserator/pkg/resourcecreator/google/sql"
+	google_storagebucket "github.com/nais/naiserator/pkg/resourcecreator/google/storagebucket"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
 )
 
-func Create(source resource.Source, ast *resource.Ast, resourceOptions resource.Options, naisGCP *nais_io_v1alpha1.GCP) error {
+func Create(source resource.Source, ast *resource.Ast, resourceOptions resource.Options, naisGCP *nais_io_v1.GCP) error {
 	if len(resourceOptions.GoogleProjectId) <= 0 {
 		return nil
 	}
@@ -19,6 +20,10 @@ func Create(source resource.Source, ast *resource.Ast, resourceOptions resource.
 	ast.AppendOperation(resource.OperationCreateOrUpdate, &googleServiceAccountBinding)
 
 	if naisGCP != nil {
+		if len(resourceOptions.GoogleTeamProjectId) == 0 {
+			return fmt.Errorf("cannot create GCP resource(s) in non team namespace")
+		}
+
 		google_storagebucket.Create(source, ast, resourceOptions, googleServiceAccount, naisGCP.Buckets)
 		err := google_sql.CreateInstance(source, ast, resourceOptions, &naisGCP.SqlInstances)
 		if err != nil {
