84f0787f80be75822102e868de74ff197173f792
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-11-24 12:21:35 +0100

    added logical rg name and fixed tests

--- pkg/skatteetaten_resourcecreator/azure/storageaccount/storageaccount.go
+++ pkg/skatteetaten_resourcecreator/azure/storageaccount/storageaccount.go
@@ -10,6 +10,7 @@ import (
 	azure_microsoft_com_v1alpha1 "github.com/nais/liberator/pkg/apis/azure.microsoft.com/v1alpha1"
 	skatteetaten_no_v1alpha1 "github.com/nais/liberator/pkg/apis/nebula.skatteetaten.no/v1alpha1"
 	"github.com/nais/naiserator/pkg/resourcecreator/resource"
+	"github.com/nais/naiserator/pkg/skatteetaten_resourcecreator/azure/resourceGroup"
 	"github.com/nais/naiserator/pkg/skatteetaten_resourcecreator/istio/service_entry"
 	"github.com/nais/naiserator/pkg/util"
 	corev1 "k8s.io/api/core/v1"
@@ -30,7 +31,7 @@ type ResourceName struct {
 
 func Create(app Source, ast *resource.Ast, options resource.Options) {
 	storageAccounts := app.GetStorageAccounts()
-	resourceGroup := app.GetAzureResourceGroup()
+	resourceGroup := resourceGroup.GenerateResourceGroupName(app)
 	subscription := options.AzureSubscriptionName
 	for _, sg := range storageAccounts {
 		generateStorageAccount(app, ast, resourceGroup, sg, subscription)
